053b48507713c0815ea3bee8644d31c408c62bba
Replit user <replituser@example.com>
2021-04-14 16:35:17 +0000

    ob

---
+++ main/progjav/Carro.java
@@ -0,0 +1,11 @@
+public class Carro{
+//Atributos de la clase
+    private static String Color= "Azul";
+    private static String Marca = "Susuki" ;
+    private static String Modelo= " 2022";
+    
+
+  public static void main(String[] argv){
+    System.out.print("Este es un carro de color "+Color+" De marca "+Marca+" Modelo "+Modelo);
+  }
+}
