07a5d97b7b44190a202d56b60f62bde1116d7878
Replit user <replituser@example.com>
2021-04-16 14:55:44 +0000

    m

--- main/progjav/Carro.java
+++ main/progjav/Carro.java
@@ -1,11 +1,16 @@
 public class Carro{
 //Atributos de la clase
-    private static String Color= "Azul";
-    private static String Marca = "Susuki" ;
-    private static String Modelo= " 2022";
+    private static String color;
+    private static String marca;
+    private static String modelo;
     
 
   public static void main(String[] argv){
-    System.out.print("Este es un carro de color "+Color+" De marca "+Marca+" Modelo "+Modelo);
+    Valores("Azul", "Susuki", "2022");
+    System.out.print("Este es un carro de color "+color+" De marca "+marca+" Modelo "+modelo);
   }
+public static void Valores(String col, String mar, String mod){
+color= col; marca=mar; modelo=mod;
+}
+
 }
